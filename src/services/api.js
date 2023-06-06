a262fc369ba1cc48c9f6cea802b79007e6e76144
gidaltibn <gidaltibn@outlook.com>
2023-06-06 19:09:00 -0300

    Inserindo a url da produção

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL:'http://10.201.30.192:8080/', //ip do tce
+    //baseURL:'http://10.201.30.192:8080/', //ip do tce
     //baseURL:'http://192.168.251.116:8080/', //ip da undb
     //baseURL:'http://192.168.3.20:8080/', //ip de casa
+    baseURL:'https://laplage-api.herokuapp.com/', //url da api
 });
 
 export default api;
